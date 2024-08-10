4cf1640016ce1282b000659c5d24cdfff88c79e9
Rayancoder21 <rayan.h.khogali@gmail.com>
2024-08-10 09:01:24 +0000

    task0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Script that computes a minimum operations
+    needed in a CopyAll - Paste task
+"""
+
+
+def minOperations(n):
+    """
+    Method for compute the minimum number
+    of operations for task Copy and paste
+    Return: the sum of the operations
+    """
+    if n < 2:
+        return 0
+    factor_list = []
+    i = 1
+    while n != 1:
+        i += 1
+        if n % i == 0:
+            while n % i == 0:
+                n /= i
+                factor_list.append(i)
+    return sum(factor_list)
